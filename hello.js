e1999fc3aeebeb6a782bedd4bd40a3cffdffd684
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-16 23:13:00 +0430

    change next arrow slide responsive

--- hello.js
+++ hello.js
@@ -33,7 +33,7 @@ export const SliderData = [
     },
     {
         id:2,
-        title:'چطوری',
+        title:'چطوری توله سگ!!',
         img:'https://media4.s-nbcnews.com/j/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.nbcnews-fp-1200-630.png',
     }
     ,

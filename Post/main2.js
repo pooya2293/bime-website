1865077c43b817ab01b17201c2b66664d391352e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-19 12:56:38 +0430

    responsive img caption

--- Post/main2.js
+++ Post/main2.js
@@ -5,8 +5,15 @@ const layer = document.querySelector(".layer");
 const ulLinks = document.querySelector('.links').querySelector('ul');
 const logo = document.querySelector('.logo').querySelector('img');
 
+const openLinks= ()=>{
+	var links = document.querySelector('.links');
+	links.classList.toggle('responsive');
+}
+const content = document.querySelector('.content');
+content.addEventListener('click',()=>document.querySelector('.links').classList.remove('responsive'));
+
 navbar.classList.add("sticky");
-layer.style.position = 'relative';
+// layer.style.position = 'relative';
 ulLinks.classList.add('linksUl-scroll');
 ulLinks.style.top = '2.2px';
 logo.style.width = '70px';

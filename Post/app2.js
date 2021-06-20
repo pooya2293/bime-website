d9b83c0def41921fb61a9f49e2a5c9b459dc8862
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-20 15:15:57 +0430

    dynamic import

--- Post/app2.js
+++ Post/app2.js
@@ -1,45 +1,63 @@
-import { Post1 } from './postData/post1.js'
+// import { result } from './postData/postTotal.js';
+// import { Post2 } from './postData/post2.js';
 
-const postInfoDOM = document.querySelector('.weblog')
-/* get weblog data */
-class Posts {
-	getPosts() {
-		return Post1;
+// import  Post2  from './postData/Post2.js';
+// import * as Post from './postData/index.js';
+
+document.addEventListener('DOMContentLoaded',async()=>{
+
+	/**** dunamic import ****/
+	const pathData = 1<2 ? './postData/Post1.js' : './postData/Post2.js';
+	const result =await import(pathData);
+	window.console.log(result.default);
+
+
+
+	/**** linking to DOM ****/
+
+	const postInfoDOM = document.querySelector('.weblog');
+	/* get weblog data */
+	 class Posts {
+		getPosts() {
+			return result.default;
+		}
 	}
-}
 
-class UI {
-	displayPostInfo(infos){
-		let result = '';
-		let pResult = '';
-		infos.forEach((info)=>{
-			result += `${info.mainImage?`<img src=${info.mainImage} alt="">`:''}
-			<div class="text">
-				<span>${info.s ? info.s:'' }</span>
-				${info.title?`<h2>${info.title}</h2>`:''}
-				
-					${info.img?`<div class="img">
-						<img src=${info.img} alt="">
-					<i>${info.i}</i>
-					</div>`:''}
+	class UI {
+		displayPostInfo(infos){
+			let result = '';
+			let pResult = '';
+			infos.forEach((info)=>{
+				result += `${info.mainImage?`<img src=${info.mainImage} alt="">`:''}
+				<div class="text">
+					<span>${info.s ? info.s:'' }</span>
+					${info.title?`<h2>${info.title}</h2>`:''}
 					
+						${info.img?`<div class="img">
+							<img src=${info.img} alt="">
+						<i>${info.i}</i>
+						</div>`:''}
+						
+					
+					${info.p.map((item)=>{
+						return `<p>${item}</p>`
+					})}
+				</div>`
+
 				
-				${info.p.map((item)=>{
-					return `<p>${item}</p>`
-				})}
-			</div>`
-
-			
-		});
-		result = result.replace(/,/g, "");//remove all coloms (,);
-		postInfoDOM.innerHTML = result ;
+			});
+			result = result.replace(/,/g, "");//remove all coloms (,);
+			postInfoDOM.innerHTML = result ;
+		}
 	}
-}
 
-document.addEventListener('DOMContentLoaded',()=>{
-	const ui = new UI();
-	// get all the Post info 
-	const posts = new Posts();
-	const postsInfos = posts.getPosts();
-	ui.displayPostInfo(postsInfos);
+	
+		const ui = new UI();
+		// get all the Post info 
+		const posts = new Posts();
+		const postsInfos = posts.getPosts();
+		ui.displayPostInfo(postsInfos);
+
 });
+
+
---
+++ Post/postData/index.js
@@ -0,0 +1,3 @@
+
+export {default as Post1} from './Post1.js';
+export {default as Post2} from './Post2.js';

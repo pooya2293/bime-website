c817ab2659a54806b1a0b9603b82db19f89d2f91
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-22 17:56:38 +0430

    some not important data chenges...

---
+++ Post/postData/Post4.js
@@ -0,0 +1,29 @@
+const Post4 =[
+	{
+		mainImage:'',
+		title:'',
+		p:[''],
+	},
+	{
+		title:'',
+		p:[''],
+	},
+	{
+		title:'',
+		p:[''],
+	},
+	{
+		title:'',
+		p:[''],
+	},
+	{
+		title:'',
+		p:[''],
+	},
+	{
+		title:'',
+		p:[''],
+	},
+	
+];
+export default Post4;
--- Post/postData/index.js
+++
@@ -1,4 +0,0 @@
-
-export {default as Post1} from './Post1.js';
-export {default as Post2} from './Post2.js';
-export {default as Post3} from './Post3.js';

8be232a2e2b600e0dc97f258492fd393f47549ae
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-22 20:23:48 +0430

    add Mix2 data for test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ export{default as Post2}from'./Post/postData/Post2.js';
 export{default as Link1}from'./Post/postData/Link1.js';
 export{default as Link2}from'./Post/postData/Link2.js';
 export{default as Mix1}from'./Post/postData/Mix1.js';
-
+export{default as Mix2}from'./Post/postData/Mix2.js';
 
